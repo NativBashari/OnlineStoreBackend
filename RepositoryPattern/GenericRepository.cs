3476711fcafbd59fd1084875d316dd3620728942
NativBashari <nativbas2000@gmail.com>
2023-01-10 14:59:20 +0200

    RepositoryImplemantationAdded

---
+++ RepositoryPattern/GenericRepository.cs
@@ -0,0 +1,39 @@
+﻿using Contracts;
+using Entities;
+
+namespace RepositoryPattern
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class 
+    {
+        protected readonly OnlineStoreDbContext
+        public void Complete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
