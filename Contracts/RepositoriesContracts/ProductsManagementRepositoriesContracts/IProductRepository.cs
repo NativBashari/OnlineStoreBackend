50fe80370e8a9e6720b0460f7bb194b898ed42a7
NativBashari <nativbas2000@gmail.com>
2023-01-10 14:47:13 +0200

    RepositoriesContractsAdded

---
+++ Contracts/RepositoriesContracts/ProductsManagementRepositoriesContracts/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Models.ProductsManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.RepositoriesContracts.ProductsManagementRepositoriesContracts
+{
+    public interface IProductRepository: IGenericRepository<Product>
+    {
+    }
+}
